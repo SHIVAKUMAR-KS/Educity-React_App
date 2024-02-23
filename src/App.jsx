c354ff2da06049bfe8f726ca5fdee71114b79cc0
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-02-23 21:09:08 +0530

    Added what we offer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Program from './components/Program/Program'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Navbar/>
       <Hero />
       <div className="container">
+        {/* /*These are happen due to props/* */}
+        <Title subtitle='OUR PROGRAM' title='What we offer'/>
         <Program/>
       </div>
       
