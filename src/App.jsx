0cb5cfe038ffefa73a81c2621145c09b55cbf7aa
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-02-23 18:09:13 +0530

    Added HEro and Program section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Program from './components/Program/Program'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero />
+      <div className="container">
+        <Program/>
+      </div>
+      
       
     </div>
   )
