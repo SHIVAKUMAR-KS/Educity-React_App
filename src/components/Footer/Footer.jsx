44872e301b54964ceda0ccfc4cb743971d4f9d2c
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-02-24 21:26:27 +0530

    updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <p>@ 2024 Edducity.All rights reserved</p>
-        <h1>Made by Shiva ❤️</h1>
+        <h1>Made by Shiva ✌️</h1>
 
         <ul>
             <li>Terms of services</li>
